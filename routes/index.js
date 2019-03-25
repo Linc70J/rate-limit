fc30bdfcaecf493ab4e1367a147300c57dd986af
linc <qulamj@gmail.com>
2019-03-25 21:38:48 +0800

    refactor: 優化code

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,21 @@
-var express = require('express');
-var mongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017";
-var router = express.Router();
+let router = require('express').Router();
+let mongoClient = require('mongodb').MongoClient;
+let url = "mongodb://localhost:27017";
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     // 取得 IP
-    var requestIP = (req.headers['x-forwarded-for'] || req.connection.remoteAddress ||
+    let requestIP = (req.headers['x-forwarded-for'] || req.connection.remoteAddress ||
         req.socket.remoteAddress || req.connection.socket.remoteAddress).replace(/^.*:/, '');
 
-    // 取得目前時間
-    var currentTime = new Date().getTime() / 1000 | 0;
+    // 取得目前時間(timestamp)
+    let currentTime = new Date().getTime() / 1000 | 0;
 
-    //連線mogodb
+    //連線 Mongodb
     mongoClient.connect(url, {useNewUrlParser: true}, function (err, db) {
         if (err) throw err;
-        var dbo = db.db("rate-limit");
-        var requestInfo = {ip: requestIP, createTime: currentTime};
+        let dbo = db.db("rate-limit");
+        let requestInfo = {ip: requestIP, createTime: currentTime};
 
         // 取得請求次數
         dbo.collection("users").find({
@@ -25,18 +24,18 @@ router.get('/', function (req, res, next) {
         }).limit(61).count(true, function (err, result) {
             if (err) throw err;
 
-            // 如果請求次數過多回傳Error
+            // 如果請求次數超過60次回傳Error
             if (result >= 60) {
                 res.status(429).send('請求次數過多！');
                 return;
             }
 
-            // 新增一筆請求紀錄
+            // 新增請求紀錄
             dbo.collection("users").insertOne(requestInfo, function (err) {
                 if (err) throw err;
                 db.close();
 
-                // 顯示IP與請求次數
+                // 顯示 IP 與 請求次數
                 res.render('index', {
                     title: 'rate-limit', ip: requestIP, count: result + 1
                 });
